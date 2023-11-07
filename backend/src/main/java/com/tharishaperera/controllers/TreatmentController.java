4d4d4a68c1dd9b9ab636822c2a0e7beb041905f8
Tharisha Perera <tharisha.perera@cammsgroup.com>
2023-11-07 21:26:00 +0530

    added missing TreatmentController

---
+++ backend/src/main/java/com/tharishaperera/controllers/TreatmentController.java
@@ -0,0 +1,53 @@
+package com.tharishaperera.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tharishaperera.models.Treatment;
+import com.tharishaperera.services.TreatmentService;
+
+@RestController
+@RequestMapping("/api/treatment")
+public class TreatmentController {
+    @Autowired
+    private TreatmentService treatmentService;
+
+    // get all treatments
+    @GetMapping
+    public List<Treatment> getAllTreatments() {
+        return treatmentService.getAllTreatments();
+    }
+
+    // get  treatment by id
+    @GetMapping("/{id}")
+    public Treatment getTreatmentById(@PathVariable Long id) {
+        return treatmentService.getTreatmentById(id);
+    }
+
+    // create treatment
+    @PostMapping
+    public Treatment createTreatment(@RequestBody Treatment treatment) {
+        return treatmentService.createTreatment(treatment);
+    }
+
+    // update treatment
+    @PutMapping("/{id}")
+    public Treatment updateTreatment(@PathVariable Long id, @RequestBody Treatment treatment) {
+        return treatmentService.updateTreatment(id, treatment);
+    }
+
+    // delete treatment 
+    @DeleteMapping("/{id}")
+    public boolean deleteTreatment(@PathVariable Long id) {
+        return treatmentService.deleteTreatment(id);
+    }
+}
